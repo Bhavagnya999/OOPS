0b9f2a9347e09190df604e1dff3dd58b7cbc18cc
Bhavagnya999 <bhavagnya939@gmail.com>
2025-02-28 10:36:11 +0530

    Add files via upload

---
+++ WEEL-1/StudentDetails.java
@@ -0,0 +1,9 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+       System.out.println("Name: Bhavagnya");
+       System.out.println("Course: B-Tech");
+       System.out.println("Semester:2");
+       System.out.println("Roll no:24209");
+    }
+}
+       
